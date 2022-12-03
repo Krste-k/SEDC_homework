a2c93cabd09d32d8627118133bab21edf7fec754
Krste-k <119616475+Krste-k@users.noreply.github.com>
2022-12-03 22:42:30 +0100

    Add files via upload

--- basic-javascript/homework1/script.js
+++ basic-javascript/homework1/script.js
@@ -1,19 +1,18 @@
-
-var priceOfPhone = 119.95;
+var priceOfOnePhone = 119.95;
+var tax = priceOfOnePhone / 20 * 1;
 var piecesOfPhones = 30;
-var sumWithOutTax = piecesOfPhones * priceOfPhone / 20 * 19;
-var sumWithTax = piecesOfPhones * priceOfPhone;
-var tax = sumWithTax - sumWithOutTax;
-var taxOfOnePice = tax / piecesOfPhones;
+var sum = priceOfOnePhone + tax;
+var sumOfAll= piecesOfPhones * priceOfOnePhone + tax;
+console.log("--------------------------------------------");
+console.log("--------------------------------------------");
+console.log("$" + sum + " is one piece with 5% tax.");
+console.log("--------------------------------------------");
+console.log("--------------------------------------------");
+console.log("$" + sumOfAll + " is cost with 5% tax for " + piecesOfPhones + " pieces.");
+console.log("--------------------------------------------");
+console.log("--------------------------------------------");
+console.log("tax per piece is $" + tax );
+console.log("--------------------------------------------");
+console.log("--------------------------------------------");
+console.log(`1 phone costs $${sum} with included tax. We sold ${piecesOfPhones} phones that cost $${sumOfAll} with included tax of 5% per piece. tax per piece is $${tax}`);
 
-console.log(sumWithTax + " Cost with tax for " + piecesOfPhones +" Phones");
-console.log("--------------------------------------------");
-console.log(sumWithOutTax  + " Cost without tax for " + piecesOfPhones +" Phones" );
-console.log("--------------------------------------------");
-console.log(tax + " total tax for " + piecesOfPhones +" Phones" );
-console.log("--------------------------------------------");
-console.log(taxOfOnePice + " total per piece");
-console.log("--------------------------------------------");
-console.log("--------------------------------------------");
-
-console.log(`1 phone costs $${priceOfPhone} with included tax. We sold ${piecesOfPhones} phones that cost ${sumWithTax} with included tax of 5% per piece. Without tax ${piecesOfPhones} phones cost ${sumWithOutTax}. The tax for ${piecesOfPhones} is ${tax} or ${taxOfOnePice} per piece.`);
